3ad08fefb613f7019e028cae4063339d71ead71a
Majorvader111 <comeon92rules@gmail.com>
2021-01-18 16:30:01 +0800

    Fix Up File Directory's and Add Source Code

---
+++ Source/NoStartupVSync/VsyncDisabler.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+
+namespace VsyncIsBad
+{
+	[KSPAddon(KSPAddon.Startup.Instantly, false)]
+	public class BadVsync : MonoBehaviour
+	{
+		public void Start()
+		{
+			QualitySettings.vSyncCount = 0; //BAD Coding Monkey = VSYNC forced on during loading which makes things load slowly.
+		}
+	}
+}
